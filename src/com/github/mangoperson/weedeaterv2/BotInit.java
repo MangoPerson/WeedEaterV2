bc13a6adc225885c3783a24611cf37d197b2a8b0
MangoPerson <79617062+MangoPerson@users.noreply.github.com>
2022-02-16 20:48:21 -0500

    Create BotInit class
    Make an init class that starts the bot and registers command listeners

---
+++ src/com/github/mangoperson/weedeaterv2/BotInit.java
@@ -0,0 +1,29 @@
+package com.github.mangoperson.weedeaterv2;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class BotInit {
+	
+	public static JDA jda;
+	
+	public static String prefix = "w!";
+	
+	public static void main(String[] args) {
+		try {
+			startBot();
+		} catch (LoginException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void startBot() throws LoginException {
+		jda = JDABuilder.createDefault("NzcwMzA3MTUwMzExNDU2Nzcw.X5bqcA.ngiqr3FitYn2c7Du33ea2HKyTvU").build();
+		jda.getPresence().setStatus(OnlineStatus.ONLINE);
+		jda.getPresence().setActivity(Activity.listening(prefix + "info"));
+	}
+}
