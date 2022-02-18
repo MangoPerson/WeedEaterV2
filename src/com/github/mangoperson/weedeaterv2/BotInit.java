476809c2efe05e789343382794e40a941d57b25e
MangoPerson <79617062+MangoPerson@users.noreply.github.com>
2022-02-17 23:19:19 -0500

    Add config support
    Create a config file and add it to the gitignore, make a config parser, and update the BotInit to remove the token from the code

--- src/com/github/mangoperson/weedeaterv2/BotInit.java
+++ src/com/github/mangoperson/weedeaterv2/BotInit.java
@@ -1,6 +1,10 @@
 package com.github.mangoperson.weedeaterv2;
 
+import java.io.IOException;
+
 import javax.security.auth.login.LoginException;
+
+import com.github.mangoperson.weedeaterv2.util.ConfigFile;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -11,18 +15,27 @@ public class BotInit {
 	
 	public static JDA jda;
 	
-	public static String prefix = "w!";
+	public static ConfigFile cfg;
+	
+	public static String prefix;
+	
+	public static String token;
 	
 	public static void main(String[] args) {
 		try {
+			cfg = new ConfigFile("config.txt");
+			prefix = cfg.valueOf("prefix");
+			token = cfg.valueOf("token");
+			System.out.println(token);
+			System.out.println("NzcwMzA3MTUwMzExNDU2Nzcw.X5bqcA.lf1W1PkhoNeF1FSgS2mHxw_4N3Y");
 			startBot();
-		} catch (LoginException e) {
+		} catch (LoginException | IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void startBot() throws LoginException {
-		jda = JDABuilder.createDefault("NzcwMzA3MTUwMzExNDU2Nzcw.X5bqcA.ngiqr3FitYn2c7Du33ea2HKyTvU").build();
+	public static void startBot() throws LoginException, IOException {
+		jda = JDABuilder.createDefault(token).build();
 		jda.getPresence().setStatus(OnlineStatus.ONLINE);
 		jda.getPresence().setActivity(Activity.listening(prefix + "info"));
 	}
